037fc038089cb4cc9361b9e62678a4c276c19e8a
Narendra Malviya <narendramalviya5@gmail.com>
2020-04-19 22:54:18 +0530

    add css in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-
+import classes from './App.css';
 class App extends Component {
  
   render() {
